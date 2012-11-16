f15e89a10bb096a68f2fd14212c735a116013fcb
Tietokone <wtl4112@rit.edu>
2012-11-16 17:04:00 -0500

    Paging works! Basic identity mapping function written, could be improved. More utility methods need to be added and the page directory structure may be changed later for Copy-on-Write.

--- src/kernel/memory/basicVirtualAllocator.d
+++ src/kernel/memory/basicVirtualAllocator.d
@@ -3,8 +3,13 @@ module kernel.memory.basicVirtualAllocator;
 import kernel.serial;
 import kernel.memory.util : memclr;
 import kernel.memory.memory;
+import kernel.interrupts;
+import kernel.support;
 import kernel.memory.iPhysicalAllocator;
 import kernel.memory.iVirtualAllocator;
+
+__gshared:
+nothrow:
 
 enum PG_PRESENT = 0x1;
 enum PG_READ_WRITE = 0x2;
@@ -12,9 +17,23 @@ enum PG_USER = 0x4;
 enum PG_WRITE_THRU = 0x8;
 enum PG_CACHE_DISABLE = 0x10;
 
+struct PageTable
+{
+	virt_addr addrs[1024];
+}
+
 struct PageDirectory
 {
-	phys_addr tables[1024];
+	union
+	{
+		phys_addr tables[1024];
+		PageTable* ptables[1024];
+	}
+
+	PageTable* get_page_table(uint index) const nothrow
+	{
+		return cast(PageTable *) (tables[index] & 0xFFFFF000);
+	}
 }
 
 class BasicVirtualAllocator : IVirtualAllocator
@@ -41,20 +60,105 @@ class BasicVirtualAllocator : IVirtualAllocator
 		return (address >> 12) & 0x03FF;
 	}
 
-	void identity_map(virt_addr low, virt_addr hi)
+	void identity_map(PageDirectory* pd, const virt_addr low, const virt_addr hi)
 	{
-		
+		// First check if there is a page table at the page directory index	
+		immutable(uint) pd_index_end = addr_to_pd_index(hi);
+
+		virt_addr current = low & 0xFFFFF000;
+		uint pd_index = addr_to_pd_index(low);
+		serial_outln("PD Start: ", pd_index, " PD End: ", cast(uint)pd_index_end);
+		serial_outln("Low: ", low, " Hi: ", hi);
+
+		do // Run as least once
+		{
+			if ((pd.tables[pd_index] & PG_PRESENT) == 0)
+			{
+				// We need to allocate a new page table, and set everything
+				// in it to zero
+				phys_addr new_page_table = m_physAllocator.allocate_page();
+				memclr(cast(void *) new_page_table, PAGE_SIZE);
+
+				// Set the directory entry and the read/write permissions
+				pd.tables[pd_index] = new_page_table;
+				pd.tables[pd_index] |= PG_PRESENT | PG_READ_WRITE;
+				serial_outln("PT: ", cast(uint) pd.tables[pd_index]);
+			}
+
+			PageTable* pt = pd.get_page_table(pd_index);
+			serial_outln("pt: ", cast(uint)pt);
+
+			// Map the addresses correctly
+			uint pt_index = addr_to_pt_index(current);	
+			while (addr_to_pd_index(current) == pd_index && 
+					current < hi) // Don't map more than we have to
+			{
+				pt.addrs[pt_index] = current | PG_READ_WRITE | PG_PRESENT;
+				
+				current += PAGE_SIZE;
+				++pt_index;
+			}
+
+			// Increment the page directory index
+			++pd_index;
+		} while(pd_index < pd_index_end);
 	}
+
+	private PageDirectory* allocate_page_directory()
+	{
+		PageDirectory* pd = cast(PageDirectory *) m_physAllocator.allocate_page();	
+		// Make sure it's zero'd out, forgetting to do this can cause all
+		// kinds of fun, hard to track down bugs
+		memclr(cast(void *) pd, PAGE_SIZE); 
+
+		return pd;
+	}
+	
 
 	void enable_paging(ref MemoryInfo info)
 	{
-		m_kernelTable = cast(PageDirectory *) m_physAllocator.allocate_page();	
+		m_kernelTable = allocate_page_directory();
 
-		memclr(m_kernelTable, PageDirectory.sizeof);
 		// Identity map the first megabyte
-		identity_map(0x0, 0x100000);
+		identity_map(m_kernelTable, 0x0, 0x100000);
 
 		// Identity map the kernel
-		identity_map(info.kernel_start, info.kernel_end);
+		identity_map(m_kernelTable, info.kernel_start, info.kernel_end);
+
+		// Don't forget to map the stack!
+		identity_map(m_kernelTable, info.kernel_end, 0x200000);
+
+		// In case we mess up
+		install_isr(INT_VEC_PAGE_FAULT, &isr_page_fault);
+
+		// Turn on paging!
+		switch_page_directory(m_kernelTable);
+		set_paging_bit();
 	}
+}
+
+void set_paging_bit()
+{
+	asm
+	{
+		mov EAX, CR0;
+		or EAX, 0x80000000;
+		mov CR0, EAX;
+	}
+}
+
+void switch_page_directory(PageDirectory* pd)
+{
+	asm
+	{
+		mov EAX, [pd];
+		mov CR3, EAX;
+	}
+}
+
+extern (C)
+void isr_page_fault(int vector, int code)
+{
+	serial_outln("Page Fault: ", vector, " ", code);
+	panic();
 }
