ead72f93d4b575e88d4c4a0bda13f22f0229716f
Tietokone <wtl4112@rit.edu>
2012-11-16 19:53:49 -0500

    Added nothrow declarations and another function map_page() which adds a new page at the specified location for the current page directory.

--- src/kernel/memory/iVirtualAllocator.d
+++ src/kernel/memory/iVirtualAllocator.d
@@ -5,7 +5,18 @@ import kernel.memory.iPhysicalAllocator;
 
 alias uint virt_addr;
 
+enum PG_PRESENT = 0x1;
+enum PG_READ_WRITE = 0x2;
+enum PG_USER = 0x4;
+enum PG_WRITE_THRU = 0x8;
+enum PG_CACHE_DISABLE = 0x10;
+
+
 interface IVirtualAllocator
 {
+nothrow:
+
 	void initialize(IPhysicalAllocator phys_allocator, ref MemoryInfo info);
+
+	void map_page(virt_addr address, uint permissions);
 }
