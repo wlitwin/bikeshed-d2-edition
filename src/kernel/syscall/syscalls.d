deae0fa8614cc615a391f38199e83b0a0da0a401
Tietokone <wtl4112@rit.edu>
2012-11-21 20:49:01 -0500

    Implemented sys_kill

--- src/kernel/syscall/syscalls.d
+++ src/kernel/syscall/syscalls.d
@@ -6,6 +6,11 @@ import kernel.interrupts;
 import kernel.process.scheduler;
 import kernel.support;
 import kernel.serial;
+import kernel.types;
+import kernel.memory.util;
+import kernel.memory.malloc : kmalloc;
+import kernel.memory.iVirtualAllocator;
+import kernel.clock : system_time;
 
 __gshared:
 nothrow:
@@ -42,6 +47,7 @@ syscalls_initialize()
 {
 	syscall_table[Syscalls.FORK]         = &sys_fork;
 	syscall_table[Syscalls.EXEC]         = &sys_exec;
+	syscall_table[Syscalls.EXIT]         = &sys_exit;
 	syscall_table[Syscalls.MSLEEP]       = &sys_msleep;
 	syscall_table[Syscalls.READ]         = &sys_read;
 	syscall_table[Syscalls.WRITE]        = &sys_write;
@@ -86,52 +92,170 @@ isr_syscall(int vector, int code)
 
 void sys_fork(ProcessControlBlock* pcb)
 {
+	ProcessControlBlock* new_pcb = cast(ProcessControlBlock*)kmalloc(ProcessControlBlock.sizeof);
+	if (new_pcb == null)
+	{
+		// Set the return value
+		pcb.context.EAX = Status.FAILURE;
+		return;
+	}
+
+	// Duplicate the parents PCB
+	memcpy(cast(void*)new_pcb, cast(void*)pcb, ProcessControlBlock.sizeof);
+
+	// Create a new page directory
+	new_pcb.page_directory = clone_page_directory();
+
+	// Fix the PCB fields
+	new_pcb.pid = next_pid();
+	new_pcb.ppid = pcb.pid;
+	new_pcb.state = State.NEW;
+
+	// Assign the PID return values for the two processes
+	uint* ptr = cast(uint*)ARG(pcb)[1];	
+	*ptr = new_pcb.pid;
+
+	// The reason the page directories need to keep switching
+	// is because the parent and child share the same virtual
+	// address for their stack. So if the page directory wasn't
+	// changed then the return value would just be overwritten
+	// for the parent and the child would never get a proper
+	// return value
+
+	// Switch the page directory
+	switch_page_directory(new_pcb.page_directory);
+	ptr = cast(uint*)ARG(new_pcb)[1];
+	*ptr = 0;
+
+	switch_page_directory(pcb.page_directory);
+
+	// Add this PCB to the master list
+	g_pcb_list.append(new_pcb);
+
+	/* We decide to schedule the child and let the parent continue
+	 */
+	Status status = schedule(new_pcb);
+	if (status != Status.SUCCESS)
+	{
+		pcb.context.EAX = Status.FAILURE;
+		cleanup(new_pcb);
+	}
+	else
+	{
+		// Set the return values for the parent and child
+		pcb.context.EAX = Status.SUCCESS;
+		switch_page_directory(new_pcb.page_directory);
+		new_pcb.context.EAX = Status.SUCCESS;
+		switch_page_directory(pcb.page_directory);
+	}
 }
 
 void sys_exec(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = Status.FEATURE_UNIMPLEMENTED;
+}
+
+void sys_exit(ProcessControlBlock* pcb)
+{
+	// Deallocate everything used by this process
+	cleanup(pcb);
+
+	// Run some other process
+	dispatch();
 }
 
 void sys_msleep(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = Status.FEATURE_UNIMPLEMENTED;
 }
 
 void sys_read(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = Status.FEATURE_UNIMPLEMENTED;
 }
 
 void sys_write(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = Status.FEATURE_UNIMPLEMENTED;
 }
 
 void sys_kill(ProcessControlBlock* pcb)
 {
+	Pid pid = cast(Pid)ARG(pcb)[1];
+
+	// Find the PCB to kill
+	// Would be super convenient if delegates/closures
+	// were able to be used!
+	struct Compare
+	{
+	nothrow:
+		int val_to_find;
+		bool compare(const ref ProcessControlBlock* pcb) const 
+		{
+			return val_to_find == pcb.pid;
+		}
+	}
+
+	Compare compare_struct;
+	compare_struct.val_to_find = pid;
+
+	ProcessControlBlock* found_pcb = null;
+
+	if (g_pcb_list.find(compare_struct, found_pcb))
+	{
+		// Mark this process as killed
+		found_pcb.state = State.KILLED;	
+		pcb.context.EAX = Status.SUCCESS;
+	}
+	else
+	{
+		// Didn't find the process
+		pcb.context.EAX = Status.NOT_FOUND;
+	}
 }
 
 void sys_get_priority(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = pcb.priority;
 }
 
 void sys_get_pid(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = pcb.pid;
 }
 
 void sys_get_ppid(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = pcb.ppid;
 }
 
 void sys_get_state(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = pcb.state;
 }
 
 void sys_get_time(ProcessControlBlock* pcb)
 {
+	pcb.context.EAX = system_time;
 }
 
 void sys_set_priority(ProcessControlBlock* pcb)
 {
+	Priority priority = cast(Priority)ARG(pcb)[1];
+
+	if (priority <= Priority.max)
+	{
+		pcb.priority = priority;	
+		pcb.context.EAX = Status.SUCCESS;
+	}
+	else
+	{
+		pcb.context.EAX = Status.FAILURE;
+	}
 }
 
 void sys_set_time(ProcessControlBlock* pcb)
 {
+	system_time = ARG(pcb)[1];
+	pcb.context.EAX = Status.SUCCESS;
 }
