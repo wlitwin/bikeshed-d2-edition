1f3d3671b1bee06b9b046e6f12ee3f4fe32cc8ee
Tietokone <wtl4112@rit.edu>
2012-11-27 22:13:36 -0500

    Forgot to remove top-level version(alloca) statement.

--- src/kernel/runtime/rt/alloca.d
+++ src/kernel/runtime/rt/alloca.d
@@ -15,9 +15,6 @@ version = alloca;
 
 // Use DMC++'s alloca() for Win32
 
-version (alloca)
-{
-
 /+
 #if DOS386
 extern size_t _x386_break;
@@ -41,126 +38,124 @@ extern size_t _pastdata;
 
 extern (C) void* __alloca(int nbytes)
 {
-  version (D_InlineAsm_X86)
-  {
-    asm
-    {
-        naked                   ;
-        mov     EDX,ECX         ;
-        mov     EAX,4[ESP]      ; // get nbytes
-        push    EBX             ;
-        push    EDI             ;
-        push    ESI             ;
+	version (D_InlineAsm_X86)
+	{
+		asm
+		{
+			naked                   ;
+			mov     EDX,ECX         ;
+			mov     EAX,4[ESP]      ; // get nbytes
+			push    EBX             ;
+			push    EDI             ;
+			push    ESI             ;
 
 
-        add     EAX,3           ;
-        and     EAX,0xFFFFFFFC  ; // round up to dword
+			add     EAX,3           ;
+			and     EAX,0xFFFFFFFC  ; // round up to dword
 
 
-        jnz     Abegin          ;
-        mov     EAX,4           ; // allow zero bytes allocation, 0 rounded to dword is 4..
-    Abegin:
-        mov     ESI,EAX         ; // ESI = nbytes
-        neg     EAX             ;
-        add     EAX,ESP         ; // EAX is now what the new ESP will be.
-        jae     Aoverflow       ;
-    }
+			jnz     Abegin          ;
+			mov     EAX,4           ; // allow zero bytes allocation, 0 rounded to dword is 4..
+Abegin:
+			mov     ESI,EAX         ; // ESI = nbytes
+			neg     EAX             ;
+			add     EAX,ESP         ; // EAX is now what the new ESP will be.
+			jae     Aoverflow       ;
+		}
 
-    version (Unix)
-    {
-    asm
-    {
-        cmp     EAX,_pastdata   ;
-        jbe     Aoverflow       ; // Unlikely - ~2 Gbytes under UNIX
-    }
-    }
-    asm
-    {
-        // Copy down to [ESP] the temps on the stack.
-        // The number of temps is (EBP - ESP - locals).
-        mov     ECX,EBP         ;
-        sub     ECX,ESP         ;
-        sub     ECX,[EDX]       ; // ECX = number of temps (bytes) to move.
-        add     [EDX],ESI       ; // adjust locals by nbytes for next call to alloca()
-        mov     ESP,EAX         ; // Set up new stack pointer.
-        add     EAX,ECX         ; // Return value = ESP + temps.
-        mov     EDI,ESP         ; // Destination of copy of temps.
-        add     ESI,ESP         ; // Source of copy.
-        shr     ECX,2           ; // ECX to count of dwords in temps
-                                  // Always at least 4 (nbytes, EIP, ESI,and EDI).
-        rep                     ;
-        movsd                   ;
-        jmp     done            ;
+		version (Unix)
+		{
+			asm
+			{
+				cmp     EAX,_pastdata   ;
+				jbe     Aoverflow       ; // Unlikely - ~2 Gbytes under UNIX
+			}
+		}
+		asm
+		{
+			// Copy down to [ESP] the temps on the stack.
+			// The number of temps is (EBP - ESP - locals).
+			mov     ECX,EBP         ;
+			sub     ECX,ESP         ;
+			sub     ECX,[EDX]       ; // ECX = number of temps (bytes) to move.
+			add     [EDX],ESI       ; // adjust locals by nbytes for next call to alloca()
+			mov     ESP,EAX         ; // Set up new stack pointer.
+			add     EAX,ECX         ; // Return value = ESP + temps.
+			mov     EDI,ESP         ; // Destination of copy of temps.
+			add     ESI,ESP         ; // Source of copy.
+			shr     ECX,2           ; // ECX to count of dwords in temps
+			// Always at least 4 (nbytes, EIP, ESI,and EDI).
+			rep                     ;
+			movsd                   ;
+			jmp     done            ;
 
-    Aoverflow:
-        // Overflowed the stack.  Return null
-        xor     EAX,EAX         ;
+Aoverflow:
+			// Overflowed the stack.  Return null
+			xor     EAX,EAX         ;
 
-    done:
-        pop     ESI             ;
-        pop     EDI             ;
-        pop     EBX             ;
-        ret                     ;
-    }
-  }
-  else version (D_InlineAsm_X86_64)
-  {
-    asm
-    {
-        /* Parameter is passed in RDI
-         * Must save registers RBX,R12..R15
-         */
-        naked                   ;
-        mov     RDX,RCX         ;
-        mov     RAX,RDI         ; // get nbytes
-        add     RAX,15          ;
-        and     AL,0xF0         ; // round up to 16 byte boundary
-        test    RAX,RAX         ;
-        jnz     Abegin          ;
-        mov     RAX,16          ; // allow zero bytes allocation
-    Abegin:
-        mov     RSI,RAX         ; // RSI = nbytes
-        neg     RAX             ;
-        add     RAX,RSP         ; // RAX is now what the new RSP will be.
-        jae     Aoverflow       ;
-    }
+done:
+			pop     ESI             ;
+			pop     EDI             ;
+			pop     EBX             ;
+			ret                     ;
+		}
+	}
+	else version (D_InlineAsm_X86_64)
+	{
+		asm
+		{
+			/* Parameter is passed in RDI
+			 * Must save registers RBX,R12..R15
+			 */
+			naked                   ;
+			mov     RDX,RCX         ;
+			mov     RAX,RDI         ; // get nbytes
+			add     RAX,15          ;
+			and     AL,0xF0         ; // round up to 16 byte boundary
+			test    RAX,RAX         ;
+			jnz     Abegin          ;
+			mov     RAX,16          ; // allow zero bytes allocation
+Abegin:
+			mov     RSI,RAX         ; // RSI = nbytes
+			neg     RAX             ;
+			add     RAX,RSP         ; // RAX is now what the new RSP will be.
+			jae     Aoverflow       ;
+		}
 
-    version (Unix)
-    {
-    asm
-    {
-        cmp     RAX,_pastdata   ;
-        jbe     Aoverflow       ; // Unlikely - ~2 Gbytes under UNIX
-    }
-    }
-    asm
-    {
-        // Copy down to [RSP] the temps on the stack.
-        // The number of temps is (RBP - RSP - locals).
-        mov     RCX,RBP         ;
-        sub     RCX,RSP         ;
-        sub     RCX,[RDX]       ; // RCX = number of temps (bytes) to move.
-        add     [RDX],RSI       ; // adjust locals by nbytes for next call to alloca()
-        mov     RSP,RAX         ; // Set up new stack pointer.
-        add     RAX,RCX         ; // Return value = RSP + temps.
-        mov     RDI,RSP         ; // Destination of copy of temps.
-        add     RSI,RSP         ; // Source of copy.
-        shr     RCX,3           ; // RCX to count of qwords in temps
-        rep                     ;
-        movsq                   ;
-        jmp     done            ;
+		version (Unix)
+		{
+			asm
+			{
+				cmp     RAX,_pastdata   ;
+				jbe     Aoverflow       ; // Unlikely - ~2 Gbytes under UNIX
+			}
 
-    Aoverflow:
-        // Overflowed the stack.  Return null
-        xor     RAX,RAX         ;
+		}
+		asm
+		{
+			// Copy down to [RSP] the temps on the stack.
+			// The number of temps is (RBP - RSP - locals).
+			mov     RCX,RBP         ;
+			sub     RCX,RSP         ;
+			sub     RCX,[RDX]       ; // RCX = number of temps (bytes) to move.
+			add     [RDX],RSI       ; // adjust locals by nbytes for next call to alloca()
+			mov     RSP,RAX         ; // Set up new stack pointer.
+			add     RAX,RCX         ; // Return value = RSP + temps.
+			mov     RDI,RSP         ; // Destination of copy of temps.
+			add     RSI,RSP         ; // Source of copy.
+			shr     RCX,3           ; // RCX to count of qwords in temps
+			rep                     ;
+			movsq                   ;
+			jmp     done            ;
 
-    done:
-        ret                     ;
-    }
-    }
-  }
-  else
-        static assert(0);
-}
+Aoverflow:
+			// Overflowed the stack.  Return null
+			xor     RAX,RAX         ;
 
+done:
+			ret                     ;
+		}
+
+	}
+	
 }
