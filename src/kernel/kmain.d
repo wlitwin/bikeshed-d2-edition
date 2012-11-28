d8c8a372862c4f0d8499691762df7103f86c6f9c
Tietokone <wtl4112@rit.edu>
2012-11-27 22:20:54 -0500

    Cleaned up kmain.d a little bit.

--- src/kernel/kmain.d
+++ src/kernel/kmain.d
@@ -1,13 +1,11 @@
 import kernel.layer0.vga;
 import kernel.layer0.serial;
 import kernel.layer0.interrupts;
-import kernel.layer1.clock;
 import kernel.layer0.memory.memory;
+
+import kernel.layer1.clock;
 import kernel.layer1.process.scheduler;
 import kernel.layer1.syscall.syscalls;
-
-// From the D runtime
-//import core.runtime;
 
 __gshared:
 string message = "Hello World! From the D2 Programming language!";
@@ -18,52 +16,19 @@ kmain()
 	put_string(0, 0, message);
 
 	init_serial_debug();
-	serial_outln("TESTING TESTING\n", 10, "\n", -10, "\n");
+
 	// Must be first
 	init_interrupts();
 	init_clock();
 	// Memory needs to happen before any other modules
 	init_memory();
-	put_string(0, 1, "Initialized Memory");
 
 	// All other modules that depend on linkedlist or memory allocation
 	scheduler_initialize();
-
 	syscalls_initialize();
 
 	serial_outln("Finished loading the kernel");
 
-/+	void exception_handler(Throwable t)
-	{
-		throw t;
-	}
-
-	/*asm { mov EAX, 0x99999999; }
-
-	void delegate(int a) nothrow my_del = void;
-
-	int super_variable = 20;
-	void my_function(int a) nothrow
-	{
-		serial_outln(super_variable, a);
-	}
-
-	my_del = &my_function;
-
-	asm { mov EAX, 0xAAAAAAAA; }
-	*/
-
-	try
-	{
-		Runtime.initialize(&exception_handler);
-
-		// Don't bother terminating the runtime
-		//Runtime.terminate();
-
-	}
-	catch (Throwable t) { /* Do nothing */ }
-	finally { /* Do nothing */ }
-	// +/
 	enable_interrupts();
 	
 	int val1 = 0;
