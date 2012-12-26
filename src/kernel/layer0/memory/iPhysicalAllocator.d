30cfc1f1ec821c4e6be0b404f946e6d2efaae909
Tietokone <wtl4112@rit.edu>
2012-12-25 23:38:01 -0500

    Revamped the physical memory manager interface. It now provides stub functions with the functionality provided by some other memory manager, like the bitmap manager.

--- src/kernel/layer0/memory/iPhysicalAllocator.d
+++ src/kernel/layer0/memory/iPhysicalAllocator.d
@@ -4,156 +4,78 @@ import kernel.layer0.serial;
 import kernel.layer0.support;
 import kernel.layer0.memory.memory;
 
+private import bitmap = kernel.layer0.memory.bitmapAllocator;
+
 // Interface for the phyiscal memory manager
-
-nothrow:
-public:
-
-alias uint phys_addr;
-/*
-extern (D)
-{
-	// Do whatever is needed to initialize
-	// the physical memory manager
-	void initialize(ref MemoryInfo info);
-
-	void reserve_page(phys_addr address);
-
-	void reserve_range(phys_addr from, phys_addr to);
-
-	// Allocate a new physical page
-	phys_addr allocate_page();
-
-	// Free a physical page
-	void free_page(phys_addr address);
-}
-*/
 
 __gshared:
 nothrow:
 public:
 
-private uint m_bitmapSize;
-private uint* m_bitmap;
-private uint m_last_index;
+alias uint phys_addr;
 
-private uint phys_to_index(phys_addr address)
+
+void
+initialize(ref MemoryInfo info)
 {
-	return cast(uint)address / PAGE_SIZE / 32;
-}
+	// Scan the memory map for some free memory
+	// above the 1MiB line that's large enough
+	// for the bitmap
+	uint bitmap_size = info.max_mem_address / PAGE_SIZE / 32;
 
-private uint phys_to_offset(phys_addr address)
-{
-	return (cast(uint)address / PAGE_SIZE) % 32;
-}
-
-void initialize(ref MemoryInfo info)
-{
-	serial_outln("\nBitmap Allocator: Initializing");
-
-	m_last_index = 0;
-
-	// Figure out how large the bitmap needs to
-	// be based on the total amount of memory available
-	m_bitmapSize = (info.memory_total / PAGE_SIZE) / 32;	
-
-	// Align it to the nearest 4-byte boundary
-	m_bitmap = cast(uint*) ((info.kernel_end + 0x4) & 0xFFFFFFF8);
-
-	// For now mark up to the bitmaps end as used
-	phys_addr end_of_bitmap = (cast(uint)m_bitmap) + 
-		m_bitmapSize * uint.sizeof;
-	// TODO - Set the individual bits instead of adding +1
-	uint end_index = phys_to_index(end_of_bitmap) + 1;
-	serial_outln("\tEnd Index: ", end_index);
-	serial_outln("\tEnd Address: ", end_of_bitmap);
-	for (int i = 0; i < end_index; ++i)
+	bool initialized = false;
+	const(MemoryMap)* mmap = info.mmap;
+	for (int i = 0; i < info.mmap_count; ++i, ++mmap)
 	{
-		m_bitmap[i] = 0xFFFFFFFF;
+		if (mmap.start > 0x100000 && mmap.length > bitmap_size)
+		{
+			// Found it!
+			initialized = true;
+			bitmap.initialize(cast(uint*) mmap.start, info.max_mem_address);
+		}
 	}
 
-	serial_outln("\tBitmap size: ", m_bitmapSize);
-	serial_outln("\tBitmap location: ", cast(uint) m_bitmap);
-	serial_outln("Bitmap Allocator: Finished\n");
-
-	// TODO update the end of the kernel
-}
-
-void reserve_page(phys_addr address)
-{
-	uint index = phys_to_index(address);
-	if (index >= m_bitmapSize)
-		return;
-
-	uint offset = phys_to_offset(address);
-	m_bitmap[index] |= (1 << offset);
-}
-
-void reserve_range(phys_addr from, phys_addr to)
-{
-	serial_outln("Bitmap Allocator: Reserving range ", cast(uint)from, " - ", cast(uint)to);
-	if (from > to)
+	if (!initialized)
 	{
-		serial_outln("Bitmap Allocator: From address is greater than To address");
-		panic();
+		panic("PHYS MM: Couldn't initialize");
 	}
 
-	while (from <= to)
+	// Loop through and free all the usable regions
+	mmap = info.mmap;
+	for (int i = 0; i < info.mmap_count; ++i, ++mmap)
 	{
-		reserve_page(from);
-		from += PAGE_SIZE;
+		// Bitmap won't free it's own storage
+		bitmap.free_range(mmap.start, mmap.start+mmap.length);
 	}
 }
 
-phys_addr allocate_page()
+phys_addr
+allocate_page()
 {
-	uint i = m_last_index;
-	uint start = i;
-	while (m_bitmap[i] == 0xFFFFFFFF)
-	{
-		i = (i + 1) % m_bitmapSize;
-		if (i == start)
-			break;
-	}
-
-	if (m_bitmap[i] == 0xFFFFFFFF)
-	{
-		// Uh-oh no more memory!
-		serial_outln("Bitmap Allocator: No more memory!");
-		panic();
-	}
-
-	// Okay find the free bit
-	uint the_bit = 0x1, offset = 0;
-	while ((m_bitmap[i] & the_bit) != 0 && the_bit != 0)
-	{
-		the_bit <<= 1;
-		++offset;
-	}
-
-	// Check if something weird happend
-	if (offset >= 32 || the_bit == 0)
-	{
-		serial_outln("Bitmap Allocator: Error finding free page!");
-		panic();
-	}
-
-	m_bitmap[i] |= the_bit;
-
-	phys_addr address = (i * PAGE_SIZE * 32) + (offset * PAGE_SIZE);
-	serial_outln("Bitmap Allocator: Giving out page ", address);
-	m_last_index = i;
-	return address;
+	return bitmap.allocate_page();
 }
 
-void free_page(phys_addr address)
+void
+reserve_region(uint start, uint length)
 {
-	uint index = phys_to_index(address);
-	if (index >= m_bitmapSize)
-		return;
-
-	serial_outln("Bitmap Allocator: Freeing page ", address);
-	uint offset = phys_to_offset(address);
-	m_bitmap[index] &= ~(1 << offset);
+	bitmap.reserve_range(start, start+length);
 }
 
+/*
+void
+unreserve_region(uint start, uint length)
+{
+}
+*/
+
+void
+free_page(phys_addr ptr)
+{
+	bitmap.free_page(cast(uint) ptr);
+}
+
+phys_addr
+allocate_pages(int num_blocks)
+{
+	return bitmap.allocate_pages(num_blocks);
+}
