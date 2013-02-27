532b9a59f94fac082f71ff2ef022366828e7410a
Tietokone <wtl4112@rit.edu>
2013-02-26 20:10:43 -0500

    Added the ability to add files. Added a helper class to walk directories and create/add files while walking.

--- utils/simpleFATEditor/editor.d
+++ utils/simpleFATEditor/editor.d
@@ -5,8 +5,6 @@ import io = std.file;
 import std.stdio;
 import std.conv;
 import std.string;
-import std.path;
-import std.c.string;
 
 class Context
 {
@@ -93,7 +91,7 @@ public:
 	 *
 	 * num_clusters - How many free clusters to look for
 	 */
-	private bool has_space(uint num_clusters)
+	private bool has_space(uint num_clusters) const
 	{
 		for (int i = 0; i < boot_record.max_fat_entries && num_clusters > 0; ++i)
 		{
@@ -111,7 +109,7 @@ public:
 	 *
 	 * start_index - The index to start searching from
 	 */
-	private uint next_free_fat_index(uint start_index)
+	private uint next_free_fat_index(uint start_index) const
 	{
 		assert(start_index < boot_record.max_fat_entries, "next_free_fat_index: argument too large");
 
@@ -131,12 +129,12 @@ public:
 	 *
 	 * data - The data to put into the file system
 	 */
-	private uint fill_data(ubyte[] data)
+	private bool fill_data(ubyte[] data, uint cluster)
 	{
 		uint num_clusters = cast(uint)(data.length / cluster_size);
 		if (!has_space(num_clusters))
 		{
-			throw new Exception("Not enough space");
+			return false;
 		}
 
 		// Get the first free cluster
@@ -168,13 +166,16 @@ public:
 
 		// Mark the the last clusters 'next' pointer to EOC
 		FAT[curCluster] = ClusterType.End_Of_Chain;
+
+		cluster = firstCluster;
+		return true;
 	}
 
 	/* Checks if a given string has the correct path
 	 * format. /some/path/to/file. It does not check
 	 * if that path actually exists
 	 */
-	private bool valid_path_string(string path)
+	private bool valid_path_string(string path) const
 	{
 		// Path must start with a slash
 		if (path.length == 0 || path[0] != '/') 
@@ -192,7 +193,7 @@ public:
 	 *
 	 * Empty string is returned if the path is invalid.
 	 */
-	private string get_directory_path(string path)
+	private string get_directory_path(string path) const
 	{
 		if (!valid_path_string(path)) return "";
 
@@ -206,7 +207,7 @@ public:
 	 * string is a valid path and doesn't end
 	 * in '/' it's a file.
 	 */
-	private bool is_file_path(string path)
+	private bool is_file_path(string path) const
 	{
 		if (!valid_path_string(path)) return false;
 
@@ -216,7 +217,7 @@ public:
 	/* Checks if the path is a directory path. 
 	 * Assumes that directories end in '/'.
 	 */
-	private bool is_directory_path(string path)
+	private bool is_directory_path(string path) const
 	{
 		if (!valid_path_string(path)) return false;
 
@@ -227,12 +228,207 @@ public:
 	 * /path/to/file -> file
 	 * Empty string is returned if the path is invalid.
 	 */
-	private string get_file_name(string path)
+	private string get_file_name(string path) const
 	{
 		if (!is_file_path(path)) return "";
 
 		return path[lastIndexOf(path, '/')+1 .. $-1];
 	}
+
+	class DirectoryWalker
+	{
+		uint curCluster;
+		Directory* dir;
+		this()
+		{
+			dir = root_directory;
+			curCluster = boot_record.root_directory;
+		}
+
+		/* Move forward a directory. Returns
+		 * False if the specified directory
+		 * does not exist, or the directory 
+		 * is invalid.
+		 */
+		bool next(string dirName)
+		{
+			// Scan for the directory name
+			if (!valid_single_name(dirName)) return false;
+
+			for (int i = 0; i < DIRS_PER_CLUSTER; ++i)
+			{
+				if (dir[i].type == DirectoryType.Directory 
+					&& dir[i].file_name == dirName)
+				{
+					curCluster = dir[i].cluster;
+					dir = cast(Directory*)cluster_address(curCluster);	
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		bool next(string dir, bool create)
+		{
+			if (create)
+			{
+				if (!next(dir))
+				{
+					// Try to create it
+					if (!createDir(dir)) return false;
+
+					return next(dir);
+				}
+
+				return true;
+			}
+			else
+			{
+				return next(dir);
+			}
+		}
+
+		bool createDir(string name)
+		{
+			// Make sure the name does not contain
+			// and slashes
+			if (!valid_single_name(name)) return false;
+
+			// TODO make sure directory doesn't already exist
+
+			// Don't muck around with the actual current directory
+			Directory* dir  = this.dir;
+			uint curCluster = this.curCluster;
+			
+			// Find an empty location
+			bool found = false;
+			for (int i = 0; i < DIRS_PER_CLUSTER; ++i)
+			{
+				if (dir[i].type == DirectoryType.Free)
+				{
+					found = true;
+					dir = cast(Directory*)&dir[i];
+					break;	
+				}
+
+				// Check if there are more directories
+				if (i == DIRS_PER_CLUSTER-1 
+					&& FAT[curCluster] != ClusterType.End_Of_Chain)
+				{
+					curCluster = FAT[curCluster];
+					i = 0;
+				}
+			}
+
+			// Couldn't find a free space, try to allocate a new cluster
+			// for directories
+			if (!found)
+			{
+				// We need to extend this directory
+				if (!has_space(1)) return false;
+
+				uint newCluster = next_free_fat_index(0);
+				zero(cluster_address(newCluster), boot_record.cluster_size);
+
+				FAT[curCluster] = newCluster;
+				FAT[newCluster] = ClusterType.End_Of_Chain;
+
+				curCluster = newCluster;
+				dir = cast(Directory*)cluster_address(curCluster);
+			}
+
+			// We need to allocate space for the new directory
+			if (!has_space(1)) return false;
+
+			uint newCluster = next_free_fat_index(0);
+			zero(cluster_address(newCluster), boot_record.cluster_size);
+			FAT[newCluster] = ClusterType.End_Of_Chain;
+
+			// Dir is set to the correct entry
+			dir.type = DirectoryType.Directory;	
+			dir.cluster = newCluster;	
+			strcopy(dir.file_name, name);
+
+			return true;
+		}
+
+		bool createFile(string name, ubyte[] data)
+		{
+			if (!valid_single_name(name)) return false;
+
+			// Find a free spot to place the file		
+			// TODO - Should check if a directory exists
+			//        with the same name?
+			Directory* dir = this.dir;
+			uint curCluster = this.curCluster;
+
+			for (int i = 0; i < DIRS_PER_CLUSTER; ++i)
+			{
+				if (dir[i].type == DirectoryType.Free)
+				{
+					// Allocate space for the file
+					uint fileCluster;
+					if (!fill_data(data, fileCluster))
+					{
+						return false;
+					}
+
+					// Place it here
+					dir[i].type = DirectoryType.File;
+					dir[i].cluster = fileCluster;
+					strcopy(dir[i].file_name, name);
+					dir[i].file_size = cast(uint)data.length;
+
+					return true;
+				}
+
+				if (i == DIRS_PER_CLUSTER-1)
+				{
+					if (FAT[curCluster] != ClusterType.End_Of_Chain)
+					{
+						curCluster = FAT[curCluster];
+					}
+					else
+					{
+						// Try to extend this directory
+						if (!has_space(1)) return false;
+
+						uint newCluster = next_free_fat_index(0);
+						FAT[curCluster] = newCluster;
+						FAT[newCluster] = ClusterType.End_Of_Chain;
+						zero(cluster_address(newCluster), boot_record.cluster_size);
+
+						curCluster = newCluster;
+					}
+
+					// Restart the loop from the beginning, curCluster has
+					// been updated appropriately
+					dir = cast(Directory*)cluster_address(curCluster);
+					i = 0;
+				}
+			}
+
+			return false;
+		}
+
+		/* Move back a directory
+		 */
+		void prev()
+		{
+			// TODO Implement
+			throw new Exception("Not Implemented");
+		}
+	}
+
+	/* Determines if the string is a valid name
+	 * for a file or directory
+	 */
+	private bool valid_single_name(const string name) const
+	{
+		return indexOf(name, '/') == -1;
+	}
+
 
 	void addFile(string path, ubyte data[])
 	{
@@ -242,10 +438,24 @@ public:
 		}
 
 		// Split the string into appropriate parts
-		string directory = get_directory_path(path);
+		string[] directories = split(get_directory_path(path), "/");
 		string filename  = get_file_name(path);
 
-		// Find the directory
+		DirectoryWalker dirWalk = new DirectoryWalker();
+		for (int i = 1; i < directories.length; ++i)
+		{
+			if (!dirWalk.next(directories[i], true))
+			{
+				// Couldn't create/move to that directory for some reason
+				throw new Exception("Couldn't add file");
+			}
+		}
+
+		// Should now be in the correct directory
+		if (!dirWalk.createFile(filename, data))
+		{
+			throw new Exception("Couldn't create file");
+		}
 	}
 
 	void write(string filename)
@@ -265,7 +475,7 @@ void zero(void* p, ulong size_in_bytes)
 	}
 }
 
-void strcopy(char dest[], string src)
+void strcopy(char[] dest, string src)
 {
 	int i = 0;
 	foreach (c ; src) 
