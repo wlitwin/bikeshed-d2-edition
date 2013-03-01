a4eab3c4845335c3b6fdc023db6e4710b29f99d3
Tietokone <wtl4112@rit.edu>
2013-02-28 21:17:06 -0500

    Added some more definitions for GDT related things. These should probably go in a different location and will most likely be moved during the refactoring.

--- kernel/src/kernel/layer0/types.d
+++ kernel/src/kernel/layer0/types.d
@@ -4,6 +4,13 @@ __gshared:
 nothrow:
 public:
 
+enum GDT_CODE = 0x0010;
+enum GDT_DATA = 0x0018;
+enum GDT_STACK = 0x0020;
+enum EFLAGS_MB1 = 0x2;
+enum EFLAGS_IF = 0x200;
+enum DEFAULT_EFLAGS = EFLAGS_MB1 | EFLAGS_IF;
+
 enum Status : uint
 {
 	SUCCESS = 0,
