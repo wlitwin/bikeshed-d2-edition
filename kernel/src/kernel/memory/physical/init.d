9fd8dc0d193d78b1f2ff423094d862c6f4802bcb
Tietokone <wtl4112@rit.edu>
2013-03-29 21:39:35 -0400

    General Physical MM calls x86 MM

--- kernel/src/kernel/memory/physical/init.d
+++ kernel/src/kernel/memory/physical/init.d
@@ -1,7 +1,22 @@
 module kernel.memory.physical.init;
 
+import kernel.memory.physical.types;
+
 __gshared:
 nothrow:
 public:
 
+private Memory g_memory;
 
+void init()
+{
+	version (X86)
+	{
+		import x86 = arch.x86.memory.physical.impl;
+		x86.init(g_memory);
+	}
+	else
+	{
+		static assert(false, "Memory Init: Unsupported Architecture");
+	}
+}
